3e9364003cb972b1c88cbd99a0c8ed881d292961
Tom Hastings <tomjuggler@gmail.com>
2017-11-12 19:49:06 +0200

    mousePressed for switching back to random patterns
    Signed-off-by: Tom Hastings <tomjuggler@gmail.com>

--- 105LEDChristmasLights.ino
+++ 105LEDChristmasLights.ino
@@ -22,6 +22,8 @@ const int buttonPin = 12;
 int outPin = 11;
 int buttonState = 0; 
 
+#define FRAMES_PER_SECOND  120
+
 void setup() { 
   delay(3000);//powerup safety delay
 	FastLED.addLeds<NEOPIXEL,DATA_PIN>(leds, NUM_LEDS);
@@ -36,70 +38,15 @@ Serial.begin(9600);
   FastLED.showColor(CRGB::Black);
 }
 
-void loop() { 
-  
-//  // read the state of the pushbutton value:
-//  buttonState = digitalRead(buttonPin);
-//
-//  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-//  if (buttonState == HIGH) {
-////    Serial.println("HIGH");
-//    
-//       // First slide the led in one direction
-//  for(int i = 0; i < NUM_LEDS; i++) {
-//    // Set the i'th led to red 
-//    leds[i] = CRGB::Red;
-//    // Show the leds
-//    FastLED.show();
-//    // now that we've shown the leds, reset the i'th led to black
-////    leds[i] = CRGB::Black;
-//    // Wait a little bit before we loop around and do it again
-////    FastLED.delay(30);
-//  }
-//   // Now go in the other direction.  
-//  for(int i = NUM_LEDS-1; i >= 0; i--) {
-//    // Set the i'th led to red 
-//    leds[i] = CRGB::Black;
-//    // Show the leds
-//    FastLED.show();
-//    // now that we've shown the leds, reset the i'th led to black
-////    leds[i] = CRGB::Black;
-//    // Wait a little bit before we loop around and do it again
-////    FastLED.delay(30);
-//  }
-//  
-//    }
-//   else {
-//    Serial.println("LOW");
-    /*
-    // First slide the led in one direction
-  for(int i = 0; i < NUM_LEDS; i++) {
-    // Set the i'th led to red 
-    leds[i] = CRGB::Blue;
-    // Show the leds
-    FastLED.show();
-    // now that we've shown the leds, reset the i'th led to black
-//    leds[i] = CRGB::Black;
-    // Wait a little bit before we loop around and do it again
-//    FastLED.delay(30);
-  }
-   // Now go in the other direction.  
-  for(int i = NUM_LEDS-1; i >= 0; i--) {
-    // Set the i'th led to red 
-    leds[i] = CRGB::Black;
-    // Show the leds
-    FastLED.show();
-    // now that we've shown the leds, reset the i'th led to black
-//    leds[i] = CRGB::Black;
-    // Wait a little bit before we loop around and do it again
-//    FastLED.delay(30);
-  }
-  */
-// }
-  
-//  FastLED.showColor(CRGB::Green);
-//  FastLED.showColor(CRGB::Black);
+// List of patterns to cycle through.  Each is defined as a separate function below.
+typedef void (*SimplePatternList[])();
+SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
 
+uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+  
+
+void loop() { 
   if(inChar=='a'){
 //    FastLED.showColor(CRGB::Green);
 // First slide the led in one direction
@@ -155,29 +102,20 @@ void loop() {
     }else if(inChar=='e'){
   
     }
-//	// First slide the led in one direction
-//	for(int i = 0; i < NUM_LEDS; i++) {
-//		// Set the i'th led to red 
-//		leds[i] = CRGB::Cyan;
-//		// Show the leds
-//		FastLED.show();
-//		// now that we've shown the leds, reset the i'th led to black
-////		leds[i] = CRGB::Black;
-//		// Wait a little bit before we loop around and do it again
-//		FastLED.delay(30);
-//	}
+    else if(inChar=='f'){
+       // Call the current pattern function once, updating the 'leds' array
+  gPatterns[gCurrentPatternNumber]();
 
-	// Now go in the other direction.  
-//	for(int i = NUM_LEDS-1; i >= 0; i--) {
-//		// Set the i'th led to red 
-//		leds[i] = CRGB::Magenta;
-//		// Show the leds
-//		FastLED.show();
-//		// now that we've shown the leds, reset the i'th led to black
-////		leds[i] = CRGB::Black;
-//		// Wait a little bit before we loop around and do it again
-//		FastLED.delay(30);
-//	}
+  // send the 'leds' array out to the actual LED strip
+  FastLED.show();  
+  // insert a delay to keep the framerate modest
+  FastLED.delay(1000/FRAMES_PER_SECOND); 
+
+  // do some periodic updates
+  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+  EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+    }
+
 
 }
 
@@ -190,3 +128,69 @@ void serialEvent() {
     
   }
 }
+
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+
+void nextPattern()
+{
+  // add one to the current pattern number, and wrap around at the end
+  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+}
+
+void rainbow() 
+{
+  // FastLED's built-in rainbow generator
+  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+}
+
+void rainbowWithGlitter() 
+{
+  // built-in FastLED rainbow, plus some random sparkly glitter
+  rainbow();
+  addGlitter(80);
+}
+
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
+  }
+}
+
+void confetti() 
+{
+  // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  int pos = random16(NUM_LEDS);
+  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+}
+
+void sinelon()
+{
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  int pos = beatsin16( 13, 0, NUM_LEDS-1 );
+  leds[pos] += CHSV( gHue, 255, 192);
+}
+
+void bpm()
+{
+  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+  uint8_t BeatsPerMinute = 62;
+  CRGBPalette16 palette = PartyColors_p;
+  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+  for( int i = 0; i < NUM_LEDS; i++) { //9948
+    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+  }
+}
+
+void juggle() {
+  // eight colored dots, weaving in and out of sync with each other
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  byte dothue = 0;
+  for( int i = 0; i < 8; i++) {
+    leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
+}
+
